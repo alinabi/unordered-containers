38ad3118968fef59c6885e4b6bd3cbc00dbe88b0
alinabi <fromgithub.10.alinabi@spamgourmet.com>
2011-03-23 19:27:32 -0400

    fixed insert bug in Data/CascadeMap/Common.hs

--- Data/CascadeMap/Common.hs
+++ Data/CascadeMap/Common.hs
@@ -59,21 +59,21 @@ singletonB h k v = Bucket h . FL.Cons k v $ FL.Nil
 {-# INLINE singletonB #-}
 
 lookupB :: Eq k => Path -> k -> Bucket k v -> Maybe v
-lookupB !h k (Bucket h' l) = go h' l where
-    go x ps | x /= h    = Nothing
-            | otherwise = FL.lookupL k ps
+lookupB !h k (Bucket h' l)
+    | h' /= h   = Nothing
+    | otherwise = FL.lookupL k l 
 {-# INLINE lookupB #-}
 
-deleteB :: Eq k => Path -> k -> Bucket k v -> Either (Bucket k v) (Bucket k v)
-deleteB !h k b@(Bucket h' l) = go h' l where
-    go x ps | x /= h    = Left b
-            | otherwise = Right . Bucket h' . FL.deleteL k $ ps
+deleteB :: Eq k => Path -> k -> Bucket k v -> Maybe (Bucket k v)
+deleteB !h k b@(Bucket h' l)
+    | h' /= h   = Nothing
+    | otherwise = Just . Bucket h' . FL.deleteL k $ l
 {-# INLINE deleteB #-}
 
-insertB :: Eq k => Path -> k -> v -> Bucket k v -> Either (Bucket k v) (Bucket k v)
-insertB h k v b@(Bucket h' l) = go h' l where
-    go x ps | x /= h    = Left b
-            | otherwise = Right . Bucket h' . FL.insertL k v $ ps
+insertB :: Eq k => Path -> k -> v -> Bucket k v -> Maybe (Bucket k v)
+insertB h k v b@(Bucket h' l)
+    | nullL l || h == h' = Just . Bucket h . FL.insertL k v $ l
+    | otherwise          = Nothing
 {-# INLINE insertB #-}
 
 
@@ -108,34 +108,44 @@ delete :: (Eq k, Hashable k) => k -> Map k v -> Map k v
 delete k m = go h m where
     !h = hash k
     go !_ Empty = Empty
-    go !p m@(Tip o) = case deleteB h k $ o `get` p of
-        Left  _ -> m
-        Right b -> Tip $ set o p b
-    go !p (Bin o l r) = case deleteB h k $ o `get` p of
-        Right b -> Bin (set o p b) l r
-        Left  _ -> case fork p of
-            0 -> let l' = go (advance p) l in Bin o l' r
-            _ -> let r' = go (advance p) r in Bin o l r' 
+    go !p t@(Tip o) = 
+        case deleteB h k $ o `get` p of
+            Just b -> Tip $ set o p b
+            _      -> t
+    go !p (Bin o l r) = 
+        case deleteB h k $ o `get` p of
+            Just b -> Bin (set o p b) l r
+            _      -> case fork p of
+                          0 -> Bin o l' r
+                          _ -> Bin o l r' 
+        where
+            l' = go (advance p) l
+            r' = go (advance p) r
 #if __GLASGOW_HASKELL__ >= 700
 {-# INLINABLE delete #-}
 #endif
 
 insert :: (Eq k, Hashable k) => k -> v -> Map k v -> Map k v
-insert k v m = go (bitSize h) h m where
+insert k v m = go h m where
     !h = hash k
-    go 0 _ m = m
-    go !_ !p Empty = Tip $ set emptyO p (singletonB h k v)
-    go !n !p (Tip o) = case insertB h k v $ o `get` p of
-        Right b -> Tip $ set o p b
-        Left  _ -> let x = go (n-4) (advance p) Empty in
-            case fork p of
-                0 -> Bin o x Empty
-                _ -> Bin o Empty x
-    go !n !p (Bin o l r) = case insertB h k v $ o `get` p of
-        Right b -> Bin (set o p b) l r
-        Left  _ -> case fork p of
-            0 -> let l' = go (n-4) (advance p) l in Bin o l' r
-            _ -> let r' = go (n-4) (advance p) r in Bin o l r'
+    go !p Empty = go p (Tip emptyO)
+    go !p t@(Tip o) = 
+        case insertB h k v $ o `get` p of
+            Just b -> Tip $ set o p b
+            _      -> case fork p of
+                          0 -> Bin o x Empty
+                          _ -> Bin o Empty x
+        where
+            x = go (advance p) Empty
+    go !p (Bin o l r) = 
+        case insertB h k v $ o `get` p of
+            Just b -> Bin (set o p b) l r
+            _      -> case fork p of
+                          0 -> Bin o l' r
+                          _ -> Bin o l r'
+        where
+            l' = go (advance p) l
+            r' = go (advance p) r
 #if __GLASGOW_HASKELL__ >= 700
 {-# INLINABLE insert #-}
 #endif
@@ -152,3 +162,8 @@ advance !p = p `shiftR` 4
 fork :: Path -> Int
 fork !p = p .&. 0x8
 {-# INLINE fork #-}
+
+nullL :: FL.List k v -> Bool
+nullL FL.Nil = True
+nullL_       = False
+{-# INLINE nullL #-}
